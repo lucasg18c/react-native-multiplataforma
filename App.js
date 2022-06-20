e82e5c70aa4109062791a3bea7ce11db1622159a
Lucas Slavik <lucasg18c@gmail.com>
2022-06-20 20:35:52 -0300

    Apuntar a pagina CG

--- App.js
+++ App.js
@@ -3,5 +3,5 @@ import React from 'react';
 import WebView from 'react-native-webview';
 
 export default function App() {
-  return <WebView source={{uri: 'http://192.168.0.106:8080'}} />;
+  return <WebView source={{uri: 'https://controlglobal.biz/'}} />;
 }
